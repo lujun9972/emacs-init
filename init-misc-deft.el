ea4fa650dbfd605ee7692d2c3b7265d128a0d8ee
lujun9972 <lujun9972@gmail.com>
2015-01-24 07:20:28 +0800

    add mobileOrg,deft,dict configure

---
+++ init-misc-deft.el
@@ -0,0 +1,5 @@
+(require 'deft)
+(setq deft-extension "org")                             ;设置默认搜索org文件
+(setq deft-directory "~/我的笔记")              ;设置搜索路径
+(setq deft-text-mode 'org-mode)                 ;编辑时，默认使用org-mode
+(setq deft-use-filename-as-title t)             ;使用文件名为标题
---
+++ init-misc-dict.el
@@ -0,0 +1,7 @@
+(require 'dictionary)
+(global-set-key [mouse-3] 'dictionary-mouse-popup-matching-words)
+(global-set-key [(control c)(d)] 'dictionary-lookup-definition)
+(global-set-key [(control c)(s)] 'dictionary-search)
+(global-set-key [(control c)(m)] 'dictionary-match-words)
+;; choose a dictionary server
+(setq dictionary-server "localhost")

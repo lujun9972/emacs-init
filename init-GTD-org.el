2f50d9077c1835ed961749c59c6dce39faed3e01
lujun9972 <lujun9972@gmail.com>
2015-01-29 07:27:49 +0800

    使用unwind-protect保证还原drag-n-drop event type的handler

--- init-GTD-org.el
+++ init-GTD-org.el
@@ -49,7 +49,8 @@
 		(setq attachment-file-path (read-file-name "请输入附件名称: " attachment-directory))
 	  (setq attachment-file-path (concat (file-name-as-directory attachment-directory)  attachment-file-name))
 	  )
-	(org-insert-link nil (concat "./" (file-relative-name attachment-file-path)) nil)))
+	(org-insert-link nil (concat "./" (file-relative-name attachment-file-path)) nil)
+	(newline)))
 
 (defun org-add-attachment-and-link (&optional attachment-file-path)
   " Org file中添加附件,并增加指向附件的链接"
@@ -64,11 +65,11 @@
 (defun org-add-attachment-and-link-drag-n-drop-event (event)
   "用darg-and-drop的方式,为Org file添加附件,并增加指向附件的链接"
   (interactive "e")
-  (let ((files (nth 2 event))
+  (let ((files (nth 2 event)))
 		(message "add %s..." files)
 		(cond ((listp files)
 			   (mapcar 'org-add-attachment-and-link files))
-			  (t (error "未知的文件列表格式"))))))
+			  (t (error "未知的文件列表格式")))))
 
 
 (defun org-add-attachment-and-link-by-drag-n-drop ()
@@ -76,9 +77,10 @@
   (interactive)
   (let (old-drag-n-drop-event-handler)
 	(setq old-drag-n-drop-event-handler (lookup-key special-event-map [drag-n-drop]))
-	(define-key special-event-map [drag-n-drop] 'org-add-attachment-and-link-drag-n-drop-event)
-	(message "可以开始拖拽要添加非附件的文件了,完成后按回车")
-	(read-char)
-	(define-key special-event-map [drag-n-drop] old-drag-n-drop-event-handler)))
+	(unwind-protect (progn
+					  (define-key special-event-map [drag-n-drop] 'org-add-attachment-and-link-drag-n-drop-event)
+					  (message "可以开始拖拽要添加非附件的文件了,完成后按回车")
+					  (read-char))
+	  (define-key special-event-map [drag-n-drop] old-drag-n-drop-event-handler))))
 
 (define-key special-event-map [drag-n-drop] 'x-dnd-handle-drag-n-drop-event)

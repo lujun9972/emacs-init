a8716a3d6853f3a2c13114bd4b69289dfa54ef44
lujw <lujw@drcbank.com>
2015-02-02 11:25:14 +0800

    澧炲姞鍑芥暟,鐢ㄤ簬浠庡涓被浼肩殑鍙€塵ode涓彧寮€鍚痯refer鐨勯偅涓?

--- init.el
+++ init.el
@@ -31,6 +31,30 @@
 			(org-babel-load-file (expand-file-name (concat sublevel ".org") level-load-path) t)))
 	 (load level t)
 	 (load sublevel t))))
+
+(defun try-enable-mode (mode-function)
+  "若能开启该mode,则开启它"
+  (if (functionp mode-function)
+	  (progn
+		(funcall mode-function 1)
+		t)
+	nil))
+
+(defun try-disable-mode (mode-function)
+  "若能关闭该mode,则关闭它"
+  (if (functionp mode-function)
+	  (progn
+		(funcall mode-function -1)
+		t)
+	nil))
+
+(defun enable-prefer-mode (&rest mode-functions)
+  "从多个类似的mode中选择启用其中一个mode
+
+该函数从前往后遍历mode-functions. 开启存在的第一个mode,其他mode全部关闭"
+  (mapcar 'try-disable-mode mode-functions)
+  (some 'try-enable-mode mode-functions))
+
 (pcase system-type
   (`windows-nt (level-load "init-windows")) ; 配置windows下使用emacs
   (`cygwin (level-load "init-cygwin")) ; 配置cygwin

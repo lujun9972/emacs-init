20bd926e7a98857b8ea9eac8cdb25f38042223a7
darksun <lujun9972@sina.com>
2015-03-02 19:43:22 +0800

    使用smart-compile代替compile进行编译

--- init-program-compile.el
+++ init-program-compile.el
@@ -12,17 +12,47 @@
 (setq compilation-scroll-output t)
 ;; 若编译无错误,则自动关闭compilation窗口
 (defun compilation-abnormally-exit-message-p (exit-message)
-  (if (string-match "exited abnormally" exit-message)
+  (if (string-match-p "exited abnormally" exit-message)
 	  t
 	nil))
-(defun compilation-kill-buffer-when-compile-success(compilation-buffer exit-message)
-  ;; (message "%s" (buffer-name compilation-buffer))
-  (message "%s" (buffer-local-value 'major-mode compilation-buffer))
-  (when  (and (not (compilation-abnormally-exit-message-p exit-message)) (string= "compilation-mode" (buffer-local-value 'major-mode compilation-buffer)))
-	(kill-buffer compilation-buffer)))
+
+(defun compilation-mode-buffer-p (buf)
+  "判断buffer是否为compilation-mode
+
+它与`compilation-buffer-p'不同之处在于`compilation-buffer-p'会对compilation-mode的子mode也返回t,而该函数返回nil"
+  (eq 'compilation-mode (buffer-local-value 'major-mode (get-buffer buf))))
+
+(defun compilation-kill-buffer-when-compile-success(buf exit-message)
+  (tooltip-show exit-message)
+  (when  (and (not (compilation-abnormally-exit-message-p exit-message))
+			  (compilation-mode-buffer-p buf))
+	(kill-buffer buf)))
 
 (add-to-list 'compilation-finish-functions #'compilation-kill-buffer-when-compile-success)
-;; F5执行编译
-(global-set-key (kbd "<f5>") 'compile)
 ;; 使用smart-compile来编译
+;; 通过smart-compile可以根据major-mode或文件名称来选择不同的compile-command
 (require-and-install 'smart-compile)
+
+;; F5执行编译
+(defun compilation-mode-buffer-alive-p ()
+  "是否有compilation-mode-buffer存在"
+  (some #'compilation-mode-buffer-p (buffer-list)))
+
+(global-set-key (kbd "<f5>") (lambda ()
+							   (interactive)
+							   (if (compilation-mode-buffer-alive-p)
+								   (recompile)
+								 (smart-compile 4)))) ;参数为4时,才重新生成compile-command,否则使用原先的compile-command
+
+
+
+
+
+
+
+
+
+
+
+
+

a5a21f717ffa88f49a51996d2cb0f4306f252d5f
lujw <lujw@drcbank.com>
2015-03-06 17:45:04 +0800

    涓轰簡浣垮緱鍚屼竴鍙版満鍣ㄤ笂涓嶅悓鐜闂翠簰琛ュ共鎵?涓烘瘡涓笉鍚岀殑绯荤粺瀹氫箟涓嶅悓鐨勪繚瀛樺悕绉?

--- init-misc-session.el
+++ init-misc-session.el
@@ -1,5 +1,7 @@
 ;;自动保存session
 (require 'desktop)
 (setq desktop-path '("."))
+;; 为每个不同的系统定义不同的保存名称,这是为了使得同一台机器上不同环境间互补干扰
+(setq desktop-base-file-name (format "%s.emacs.desktop" system-type))
 (desktop-save-mode 1)
 (toggle-save-place-globally 1)			;保存各buffer的光标位置

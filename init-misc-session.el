b24d3d1f0aa19848131e70bd44222abc2fb86ea7
lujun9972 <lujw@drcbank.com>
2015-03-07 08:04:37 +0800

    当system-type为gnu/linux或gnu/kfreebsd时,需要去掉前面的gnu/,否则会被认为是目录信息

--- init-misc-session.el
+++ init-misc-session.el
@@ -2,6 +2,6 @@
 (require 'desktop)
 (setq desktop-path '("."))
 ;; 为每个不同的系统定义不同的保存名称,这是为了使得同一台机器上不同环境间互补干扰
-(setq desktop-base-file-name (format "%s.emacs.desktop" system-type))
+(setq desktop-base-file-name (file-name-nondirectory (format "%s.emacs.desktop" system-type)))
 (desktop-save-mode 1)
 (toggle-save-place-globally 1)			;保存各buffer的光标位置

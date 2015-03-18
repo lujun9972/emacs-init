0e6ea31319cb7d9a2c227dfea1c61c2379747d7b
darksun <lujun9972@sina.com>
2015-03-18 19:57:18 +0800

    修改cygwin下load-path

--- init-cygwin.el
+++ init-cygwin.el
@@ -1,4 +1,6 @@
+(add-to-list 'load-path "~/MyLisp")
+(add-to-list 'load-path "~/emacs-init")
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/magit")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/git-modes")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/evil")
+
+(org-babel-load-file "~/CodeLibrary/elisp.org")

2d1d58260961a745d8974afe2d26bbb9f3a62889
lujun9972 <lujw@drcbank.com>
2015-03-18 23:14:34 +0800

    在emacs-init中设置上传远程的信息

--- init-misc-tramp.el
+++ init-misc-tramp.el
@@ -5,4 +5,8 @@
   (`cygwin (add-to-list 'load-path "~/MyLisp/")) ; 配置cygwin
   (`gnu/linux (add-to-list 'load-path "~/MyLisp/")))
 
+(setq autoftp-local-remote-root-alist '(("e:/git-svn/server/" . "/cnaps2@10.8.6.10:")
+									 ("/cygdrive/e/git-svn/server/" . "/cnaps2@10.8.6.10:")
+									 ("~/trunk/" . "/cnaps2@10.8.6.10:")
+									 ("d:/workcvs/ibps/ibps" . "/ibpsusr@10.8.6.10:")))
 (require 'autoftp)						;开启自动上传功能

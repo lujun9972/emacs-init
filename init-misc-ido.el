cfd250fb2e493a2ba82e3341a8aeb548590988f7
darksun <lujun9972@sina.com>
2015-03-25 20:25:19 +0800

    Use the current window when visiting files and buffers with ido

--- init-misc-ido.el
+++ init-misc-ido.el
@@ -1,14 +1,18 @@
 
 ;; 开启ido-mode
 (when (> emacs-major-version 21)
-  (ido-mode 1)
+  (ido-mode 'both)
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t 
         ido-everywhere t
+		ido-max-directory-size 100000
         ido-create-new-buffer 'always
+		;; Use the current window when visiting files and buffers with ido
 		ido-default-file-method 'selected-window
+		ido-default-buffer-method 'selected-window
         ido-use-filename-at-point nil)
   
   (add-to-list 'ido-ignore-directories "\\`\\.git/")
   (add-to-list 'ido-ignore-files "\\~\\`")
   (add-to-list 'ido-ignore-files "\\.doc\\`"))
+

762c4d378d965078a5678ce4bc6f4ddcb45ac9a8
darksun <lujw@drcbank.com>
2015-04-04 17:34:37 +0800

    屏蔽magit 1.4的警告信息

--- init-program-vc.el
+++ init-program-vc.el
@@ -3,3 +3,5 @@
 (add-to-list 'load-path "D:/program files/emacs-24.3/site-lisp/git-modes")
 (add-to-list 'load-path "D:/program files/emacs-24.3/site-lisp/magit")
 (require-and-install 'magit)
+(setq magit-auto-revert-mode nil)
+(setq magit-last-seen-setup-instructions "1.4.0")

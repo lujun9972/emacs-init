3c9f97c3bd1da3318fdb381afbb52d1c5826917b
lujun9972 <lujw@drcbank.com>
2015-04-04 23:08:44 +0800

    moved to init-program-lisp.org

--- init-program-lisp.el
+++
@@ -1,9 +0,0 @@
-
-;;eldoc编辑elisp时提示
-(require 'eldoc)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-
-;;设置lisp的命令地址
-(setq inferior-lisp-program "C:/Program Files/clisp-2.49/clisp.exe")

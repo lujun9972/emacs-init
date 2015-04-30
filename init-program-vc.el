8a208db17976413304fb0cf5362b6d3faf56c9c1
darksun <lujw@drcbank.com>
2015-04-30 17:33:09 +0800

    使用git-timestamp查阅buffer file的历史版本

--- init-program-vc.el
+++
@@ -1,9 +0,0 @@
-;; 配置magit
-(sudo-apt-get-install "git")
-(add-to-list 'load-path "D:/program files/emacs-24.3/site-lisp/git-modes")
-(add-to-list 'load-path "D:/program files/emacs-24.3/site-lisp/magit")
-(require-and-install 'magit)
-(setq magit-auto-revert-mode nil)
-(setq magit-last-seen-setup-instructions "1.4.0")
-
-(provide 'init-program-vc)

395e1899aa96532bc9a2c49f0b24fb1b3414bb28
darksun <lujw@drcbank.com>
2015-04-30 17:36:58 +0800

    使用sunrise-commander作为totol commander的代替

--- init-file-management.el
+++ init-file-management.el
@@ -1,5 +1,9 @@
 ;;配置dired
 (level-require "init-file-management-dired")
+;; 配置sunrise-commander
+(add-to-list 'package-archives '("SC"   . "http://joseito.republika.pl/sunrise-commander/")) ;sunrise-commander的安装源
+(package-install-new 'sunrise-commander)
+(package-install-new 'sunrise-x-buttons)
 ;;使用pdf2txt查看pdf
 (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf2txt)) 
 (defun pdf2txt () 

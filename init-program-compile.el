9be4236909acbc54fa0dfb5b214c684d5f6ef508
darksun <lujun9972@sina.com>
2015-05-11 21:50:13 +0800

    为了维护不同语言之间的相同操作,使用gtags代替cedet

--- init-program-compile.el
+++ init-program-compile.el
@@ -60,4 +60,86 @@
 ;; 								   (recompile)
 ;; 								 (smart-compile 1))))
 
+;; 使用smart-compile来编译
+;; 通过smart-compile可以根据major-mode或文件名称来选择不同的compile-command
+(require-and-install 'smart-compile)
+;; 编译sendtonps
+(add-to-list 'smart-compile-alist
+			 '("sendnps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/nps;make sendtonps\""))
+(add-to-list 'smart-compile-alist
+			 '("sendnps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/nps;make sendtonps\""))
+
+;; 编译recvfromnps
+(add-to-list 'smart-compile-alist
+			 '("recvnps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/nps;make recvfromnps\""))
+(add-to-list 'smart-compile-alist
+			 '("recvnps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/nps;make recvfromnps\""))
+
+;; 编译sendtobeps
+(add-to-list 'smart-compile-alist
+			 '("sendbeps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/beps;make sendtobeps\""))
+(add-to-list 'smart-compile-alist
+			 '("sendbeps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/beps;make sendtobeps\""))
+
+;; 编译recvfrombeps
+(add-to-list 'smart-compile-alist
+			 '("recvbeps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/beps;make recvfrombeps\""))
+(add-to-list 'smart-compile-alist
+			 '("recvbeps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/beps;make recvfrombeps\""))
+
+;; 编译sendtohvps
+(add-to-list 'smart-compile-alist
+			 '("sendhvps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/hvps;make sendtohvps\""))
+(add-to-list 'smart-compile-alist
+			 '("sendhvps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/hvps;make sendtohvps\""))
+
+;; 编译recvfromhvps
+(add-to-list 'smart-compile-alist
+			 '("recvhvps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/hvps;make recvfromhvps\""))
+(add-to-list 'smart-compile-alist
+			 '("recvhvps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/hvps;make recvfromhvps\""))
+
+;; 编译sendtoccms
+(add-to-list 'smart-compile-alist
+			 '("sendccms.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/ccms;make sendtoccms\""))
+(add-to-list 'smart-compile-alist
+			 '("sendccms.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/ccms;make sendtoccms\""))
+
+;; 编译recvfromccms
+(add-to-list 'smart-compile-alist
+			 '("recvccms.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/ccms;make recvfromccms\""))
+(add-to-list 'smart-compile-alist
+			 '("recvccms.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/ccms;make recvfromccms\""))
+
+;; 编译sendtosaps
+(add-to-list 'smart-compile-alist
+			 '("sendsaps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/saps;make sendtosaps\""))
+(add-to-list 'smart-compile-alist
+			 '("sendsaps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/saps;make sendtosaps\""))
+
+;; 编译recvfromsaps
+(add-to-list 'smart-compile-alist
+			 '("recvsaps.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/saps;make recvfromsaps\""))
+(add-to-list 'smart-compile-alist
+			 '("recvsaps.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/saps;make recvfromsaps\""))
+
+;; 编译sendtomb
+(add-to-list 'smart-compile-alist
+			 '("sendmb.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/mb;make sendtomb\""))
+(add-to-list 'smart-compile-alist
+			 '("sendmb.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/mb;make sendtomb\""))
+
+;; 编译recvfrommb
+(add-to-list 'smart-compile-alist
+			 '("recvmb.+\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/mb;make recvfrommb;make recvfromclt\""))
+(add-to-list 'smart-compile-alist
+			 '("recvmb.+\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/mb;make recvfrommb;make recvfromclt\""))
+
+
+;; 编译mbcharge
+(add-to-list 'smart-compile-alist
+			 '("mbcharge\.cpp$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/mb;make\""))
+(add-to-list 'smart-compile-alist
+			 '("mbcharge\.h$" . "plink cnaps2@10.8.6.10 \". ~/.profile;cd app/mb;make\""))
+
 (provide 'init-program-compile)

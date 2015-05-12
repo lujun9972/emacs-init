a3cb9b15432ab5514deebf338806f0096b8f9753
lujw <lujw@drcbank.com>
2015-05-12 09:49:47 +0800

    鏂扮殑autoftp,UpMeToFtp鏀寔C-u淇敼tramp鐨勮繙绋媔p

--- init-misc-tramp.el
+++ init-misc-tramp.el
@@ -5,10 +5,10 @@
   (`cygwin (add-to-list 'load-path "~/MyLisp/")) ; 配置cygwin
   (`gnu/linux (add-to-list 'load-path "~/MyLisp/")))
 
-(setq autoftp-local-remote-root-alist '(("e:/git-svn/server/" . "/cnaps2@10.8.6.10:")
-									 ("/cygdrive/e/git-svn/server/" . "/cnaps2@10.8.6.10:")
-									 ("~/trunk/" . "/cnaps2@10.8.6.10:")
-									 ("d:/workcvs/ibps/ibps" . "/ibpsusr@10.8.6.10:")))
+(setq autoftp-local-remote-root-alist '(("e:/git-svn/server/" "cnaps2" "10.8.6.10" "")
+									 ("/cygdrive/e/git-svn/server/" "cnaps2" "10.8.6.10" "")
+									 ("~/trunk/" "cnaps2" "10.8.6.10" "")
+									 ("d:/workcvs/ibps/ibps" "ibpsusr" "10.8.6.10" "")))
 (require 'autoftp)						;开启自动上传功能
 
 (provide 'init-misc-tramp)

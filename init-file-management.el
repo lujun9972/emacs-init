d092ac9ed1bac543b1251a9de5462843ba656706
lujun9972 <lujw@drcbank.com>
2015-06-07 04:00:26 +0800

    从purcell的配置文件中借用一些处理buffer当前文件的函数
    delete-this-file
    rename-this-file-and-buffer
    browse-current-file

--- init-file-management.el
+++ init-file-management.el
@@ -1,7 +1,40 @@
+;; 对buffer当前文件的一些处理
+(defun delete-this-file ()
+  "Delete the current file, and kill the buffer."
+  (interactive)
+  (or (buffer-file-name) (error "No file is currently being edited"))
+  (when (yes-or-no-p (format "Really delete '%s'?"
+                             (file-name-nondirectory buffer-file-name)))
+    (delete-file (buffer-file-name))
+    (kill-this-buffer)))
+
+(defun rename-this-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (unless filename
+      (error "Buffer '%s' is not visiting a file!" name))
+    (if (get-buffer new-name)
+        (message "A buffer named '%s' already exists!" new-name)
+      (progn
+        (when (file-exists-p filename)
+         (rename-file filename new-name 1))
+        (rename-buffer new-name)
+        (set-visited-file-name new-name)))))
+
+(defun browse-current-file ()
+  "Open the current file as a URL using `browse-url'."
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (if (tramp-tramp-file-p file-name)
+        (error "Cannot open tramp file")
+      (browse-url (concat "file://" file-name)))))
+
 ;;配置dired
 (level-require "init-file-management-dired")
 ;; 配置sunrise-commander
-;; (level-require "init-file-management-sunrise-commander")
+(level-require "init-file-management-sunrise-commander")
 ;;使用pdf2txt查看pdf
 (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf2txt)) 
 (defun pdf2txt () 

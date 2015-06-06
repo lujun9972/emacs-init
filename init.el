34c1150fb8b93e53a23e59658398e8a5ab2f778c
lujun9972 <lujw@drcbank.com>
2015-06-07 04:02:40 +0800

    增加melpa-stable仓库

--- init.el
+++ init.el
@@ -14,8 +14,8 @@
 ;; (add-to-list 'package-archives
 ;; 	'("marmalade" .
 ;; 	  "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
 ;; 定义几个常用目录的路径

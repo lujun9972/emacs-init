63bb91135a2fbc139fc00891d10e3a1adb718185
lujun9972 <lujw@drcbank.com>
2015-06-07 22:41:57 +0800

    使用ido-ubiquitous与idomenu

--- init-misc-ido.el
+++ init-misc-ido.el
@@ -9,12 +9,18 @@
 		;; Use the current window when visiting files and buffers with ido
 		ido-default-file-method 'selected-window
 		ido-default-buffer-method 'selected-window
-        ido-use-filename-at-point nil)
+        ido-use-filename-at-point nil
+		ido-auto-merge-work-directories-length 0
+		ido-use-virtual-buffers t)
   
   (add-to-list 'ido-ignore-directories "\\`\\.git/")
   (add-to-list 'ido-ignore-files "\\~\\`")
   (add-to-list 'ido-ignore-files "\\.doc\\`"))
-
+;; Use ido (nearly) everywhere
+(package-install-new 'ido-ubiquitous)
+(ido-ubiquitous-mode t)
+;; imenu tag selection with ido
+(package-install-new 'idomenu)
 
 ;; 若打开文件没有权限,自动使用sudo方式打开
 (defun alternate-current-file-as-root (&rest args)

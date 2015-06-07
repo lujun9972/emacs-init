3b5206c9c45a64eb59cf2a3faa74c8aa74d6d87c
lujun9972 <lujw@drcbank.com>
2015-06-07 23:36:00 +0800

    设置10分钟保存一次,并且保存minbuffer的历史

--- init-misc-session.el
+++ init-misc-session.el
@@ -3,7 +3,9 @@
 (setq desktop-path '("."))
 ;; 为每个不同的系统定义不同的保存名称,这是为了使得同一台机器上不同环境间互补干扰
 (setq desktop-base-file-name (file-name-nondirectory (format "%s.emacs.desktop" system-type)))
+(setq desktop-auto-save-timeout 600)	;10分钟保存一次
 (desktop-save-mode 1)
 (toggle-save-place-globally 1)			;保存各buffer的光标位置
+(savehist-mode t)						;保存minibuffer的history
 
 (provide 'init-misc-session)

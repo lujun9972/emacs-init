1cc24b411548b5b9ea7ce86ffc4b93cf8fc9e65d
unknown <lujw@drcbank.com>
2015-06-29 12:55:34 +0800

    鏇存敼鏍煎紡涓篣TF-8-UNIX鏍煎紡

--- init-misc-tramp.el
+++ init-misc-tramp.el
@@ -4,6 +4,7 @@
 (setq autoftp-local-remote-root-alist '(("e:/git-svn/server/" "cnaps2" "10.8.6.10" "")
 									 ("/cygdrive/e/git-svn/server/" "cnaps2" "10.8.6.10" "")
 									 ("~/trunk/" "cnaps2" "10.8.6.10" "")
+									 ("d:/workcvs/cnaps2/server/trunk/" "cnaps2" "10.8.6.10" "")
 									 ("d:/workcvs/ibps/ibps" "ibpsusr" "10.8.6.10" "")))
 (require 'autoftp)						;开启自动上传功能
 
